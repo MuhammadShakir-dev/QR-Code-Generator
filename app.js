3210f0660e20ac2f1d092e743c217ca9d82d28ea
Muhammad Shakir <chaudhryshakir67@gmail.com>
2024-09-07 13:45:19 +0500

    JavaScript source code file

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const userInput = document.querySelector('.userInput');
+const button = document.querySelector('button');
+const qrHolder = document.querySelector('#ImgBox');
+const qrImage = document.querySelector('#qrImage');
+
+
+function generateQR() {
+    if (userInput.value.length > 0) {
+        qrImage.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userInput.value}`
+        qrHolder.classList.add('showImage')
+    }
+    else { 
+        userInput.classList.add('error')
+        setTimeout(() => { 
+            userInput.classList.remove('error')
+        }, 1000)
+    }
+    
+}
+
+
 
